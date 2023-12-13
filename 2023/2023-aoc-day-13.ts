25acff363f86e0ff1b01cfdaaeaa84d660fbc5b1
Mike O'Beirne <michael.obeirne@gmail.com>
2023-12-12 19:19:45 -0500

    Change gitignore

---
+++ 2023/2023-aoc-day-13.ts
@@ -0,0 +1,20 @@
+import * as utils from '../utils/aoc-utils';
+import { Solution } from '../utils/solution';
+
+
+class Day0_2023 extends Solution {
+  DAY = 0;
+
+  partOne(input: string[]): any {
+  }
+
+  partTwo(input: string[]): any {
+  }
+
+}
+
+let sol = new Day0_2023();
+utils.runTest(sol, utils.ProblemParts.One);
+utils.run(sol, utils.ProblemParts.One);
+utils.runTest(sol, utils.ProblemParts.Two);
+utils.run(sol, utils.ProblemParts.Two);
