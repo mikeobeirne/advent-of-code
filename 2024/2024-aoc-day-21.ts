56ca471471d8cd3d93c1656c5d2b704c07e21b2c
Michael O'Beirne <michael.obeirne@gmail.com>
2024-12-16 21:04:02 -0500

    Seed with empty solutions

---
+++ 2024/2024-aoc-day-21.ts
@@ -0,0 +1,17 @@
+import * as utils from "../utils/aoc-utils";
+import { Solution } from "../utils/solution";
+
+class Day16_2024 extends Solution {
+  DAY = 16;
+
+  partOne(input: string[]): any {}
+
+  partTwo(input: string[]): any {}
+}
+
+let sol = new Day16_2024();
+
+utils.runTest(sol, utils.ProblemParts.One);
+utils.runSolution(sol, utils.ProblemParts.One);
+utils.runTest(sol, utils.ProblemParts.Two);
+utils.runSolution(sol, utils.ProblemParts.Two);
