70a40b87d618c309796ee6f340217a9e46472356
Michael O'Beirne <michael.obeirne@gmail.com>
2024-12-18 23:03:11 -0500

    Day 16 & 17
    Fuck a bigint

--- 2024/2024-aoc-day-16.ts
+++ 2024/2024-aoc-day-16.ts
@@ -1,10 +1,210 @@
 import * as utils from "../utils/aoc-utils";
 import { Solution } from "../utils/solution";
 
+type Point = {
+  x: number;
+  y: number;
+};
+
+enum Direction {
+  UP = 0,
+  RIGHT = 1,
+  DOWN = 2,
+  LEFT = 3,
+}
+
+type State = {
+  p: Point;
+  d: Direction;
+  cost: number;
+  previous: State | null;
+};
+
 class Day16_2024 extends Solution {
   DAY = 16;
 
-  partOne(input: string[]): any {}
+  partOne(input: string[]): any {
+    let start = this.findStart(input);
+    let queue: State[] = [
+      { p: start, d: Direction.RIGHT, cost: 0, previous: null },
+    ];
+
+    const dirLookup: Map<Direction, number[]> = new Map([
+      [Direction.DOWN, [1, 0]],
+      [Direction.UP, [-1, 0]],
+      [Direction.LEFT, [0, -1]],
+      [Direction.RIGHT, [0, 1]],
+    ]);
+
+    let cache: Map<number, number>[] = [
+      new Map(),
+      new Map(),
+      new Map(),
+      new Map(),
+    ];
+    let best = [];
+    let bestVal: null | number = null;
+
+    while (queue.length !== 0) {
+      let cur = queue.pop()!;
+      // console.log(
+      //   `${cur.p.x}, ${cur.p.y} (${cur.cost}) ${this.getDirectionChar(cur.d)}`
+      // );
+      // this.printCurPath(input, cur);
+
+      let cacheKey = input.length * cur.p.x + cur.p.y;
+
+      // If we've already been here more cheaply, bail
+      if (cache[cur.d].has(cacheKey)) {
+        if (cache[cur.d].get(cacheKey)! < cur.cost) {
+          continue;
+        }
+      }
+      cache[cur.d].set(cacheKey, cur.cost);
+
+      if (bestVal !== null) {
+        if (cur.cost > bestVal) {
+          continue;
+        }
+      }
+
+      if (input[cur.p.x][cur.p.y] === "E") {
+        console.log(`${cur.p.x}, ${cur.p.y} (${cur.cost})`);
+        this.printCurPath(input, cur);
+        best.push(cur);
+        bestVal = cur.cost;
+        console.log(`Best val set to ${bestVal}`);
+        continue;
+      }
+      let dir = dirLookup.get(cur.d)!;
+      let newX = cur.p.x + dir[0];
+      let newY = cur.p.y + dir[1];
+      // Walk forward?
+      if (this.isValid(input, newX, newY)) {
+        queue.push({
+          p: { x: newX, y: newY },
+          d: cur.d,
+          cost: cur.cost + 1,
+          previous: cur,
+        });
+      }
+      // Rotate right
+      queue.push({
+        p: cur.p,
+        d: (cur.d + 1) % 4,
+        cost: cur.cost + 1000,
+        previous: cur,
+      });
+
+      // Rotate left
+      queue.push({
+        p: cur.p,
+        // 3 rights makes a left lolz
+        d: (cur.d - 1 + 4) % 4,
+        cost: cur.cost + 1000,
+        previous: cur,
+      });
+
+      queue = queue.sort((a, b) => b.cost - a.cost);
+    }
+
+    console.log(best);
+    let ansCache = this.countBest(input, best);
+    this.printAnswer(input, ansCache);
+
+    return ansCache.size;
+  }
+
+  getDirectionChar(d: Direction): string {
+    return ["^", ">", "v", "<"][d];
+  }
+
+  printCurPath(input: string[], state: State): void {
+    let path: Set<number> = new Set();
+    let cur: State | null = state;
+    while (cur != null) {
+      path.add(cur.p.x * input.length + cur.p.y);
+      cur = cur.previous;
+    }
+
+    for (let i = 0; i < input.length; i++) {
+      let buf = "";
+      for (let j = 0; j < input[0].length; j++) {
+        if (i === state.p.x && j === state.p.y) {
+          buf += "@";
+        } else if (path.has(i * input.length + j)) {
+          buf += "O";
+        } else {
+          buf += input[i].charAt(j);
+        }
+      }
+      console.log(buf);
+    }
+  }
+
+  printAnswer(input: string[], answer: Set<number>): void {
+    for (let i = 0; i < input.length; i++) {
+      let buf = "";
+      for (let j = 0; j < input[0].length; j++) {
+        if (answer.has(i * input.length + j)) {
+          buf += "O";
+        } else {
+          buf += input[i].charAt(j);
+        }
+      }
+      console.log(buf);
+    }
+  }
+
+  countBest(input: string[], best: State[]): Set<number> {
+    let cache: Set<number> = new Set();
+    for (let i = 0; i < best.length; i++) {
+      let cur: State | null = best[i];
+      while (cur !== null) {
+        cache.add(cur.p.x * input.length + cur.p.y);
+        cur = cur.previous;
+      }
+    }
+
+    return cache;
+  }
+
+  getCacheKey(input: string[], s: State): number {
+    let cacheKey = s.p.x * input.length + s.p.y;
+    cacheKey += input.length * input[0].length * s.d;
+    return cacheKey;
+  }
+
+  rotateRight(d: Direction): Direction {
+    switch (d) {
+      case Direction.UP:
+        return Direction.RIGHT;
+      case Direction.RIGHT:
+        return Direction.DOWN;
+      case Direction.DOWN:
+        return Direction.LEFT;
+      case Direction.LEFT:
+        return Direction.UP;
+    }
+  }
+
+  isValid(input: string[], i: number, j: number): boolean {
+    if (i < 0 || j < 0 || i >= input.length || j >= input[0].length) {
+      return false;
+    }
+    return input[i].charAt(j) !== "#";
+  }
+
+  findStart(input: string[]): Point {
+    for (let i = 0; i < input.length; i++) {
+      for (let j = 0; j < input[0].length; j++) {
+        if (input[i].charAt(j) === "S") {
+          return { x: i, y: j };
+        }
+      }
+    }
+    throw new Error("no start");
+  }
 
   partTwo(input: string[]): any {}
 }
@@ -13,5 +213,5 @@ let sol = new Day16_2024();
 
 utils.runTest(sol, utils.ProblemParts.One);
 utils.runSolution(sol, utils.ProblemParts.One);
-utils.runTest(sol, utils.ProblemParts.Two);
-utils.runSolution(sol, utils.ProblemParts.Two);
+// utils.runTest(sol, utils.ProblemParts.Two);
+// utils.runSolution(sol, utils.ProblemParts.Two);
--- 2024/2024-aoc-day-17.ts
+++ 2024/2024-aoc-day-17.ts
@@ -1,17 +1,265 @@
 import * as utils from "../utils/aoc-utils";
 import { Solution } from "../utils/solution";
 
-class Day16_2024 extends Solution {
-  DAY = 16;
+enum INSTRUCTION {
+  // The adv instruction (opcode 0) performs division. The numerator is the value in the A register.
+  // The denominator is found by raising 2 to the power of the instruction's combo operand.
+  // (So, an operand of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.)
+  // The result of the division operation is truncated to an integer and then written to the A register.
+  ADV = 0,
 
-  partOne(input: string[]): any {}
+  // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand,
+  // then stores the result in register B.
+  BXL = 1,
 
-  partTwo(input: string[]): any {}
+  // The bst instruction (opcode 2) calculates the value of its
+  // combo operand modulo 8 (thereby keeping only its lowest 3 bits), then writes that value to the B register.
+  BST = 2,
+
+  // The jnz instruction (opcode 3) does nothing if the A register is 0.
+  // However, if the A register is not zero, it jumps by setting the instruction pointer
+  // to the value of its literal operand; if this instruction jumps,
+  // the instruction pointer is not increased by 2 after this instruction.
+  JNZ = 3,
+
+  // The bxc instruction (opcode 4) calculates the bitwise XOR of register B and register C,
+  // then stores the result in register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+  BXC = 4,
+
+  // The out instruction (opcode 5) calculates the value of its combo operand modulo 8,
+  // then outputs that value. (If a program outputs multiple values, they are separated by commas.)
+  OUT = 5,
+
+  // The bdv instruction (opcode 6) works exactly like the adv instruction except
+  // that the result is stored in the B register. (The numerator is still read from the A register.)
+  BDV = 6,
+
+  // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the C register. (The numerator is still read from the A register.)
+  CDV = 7,
 }
 
-let sol = new Day16_2024();
+type State = {
+  A: bigint;
+  B: bigint;
+  C: bigint;
+  ptr: number;
+};
 
-utils.runTest(sol, utils.ProblemParts.One);
-utils.runSolution(sol, utils.ProblemParts.One);
-utils.runTest(sol, utils.ProblemParts.Two);
-utils.runSolution(sol, utils.ProblemParts.Two);
+class Day17_2024 extends Solution {
+  DAY = 17;
+
+  public numbers: bigint[] = [];
+  public goalAns: number[] = [2, 4, 1, 3, 7, 5, 4, 2, 0, 3, 1, 5, 5, 5, 3, 0];
+
+  partOne(input: string[]): any {
+    this.numbers = [];
+
+    let A: bigint = BigInt(input[0].split(" ")[2]);
+    let B = parseInt(input[1].split(" ")[2]);
+    let C = parseInt(input[2].split(" ")[2]);
+
+    return this.fullEval(A);
+  }
+
+  fullEval(a: bigint): string {
+    let p = "Program: 2,4,1,3,7,5,4,2,0,3,1,5,5,5,3,0";
+    return this.eval(a, p);
+  }
+
+  eval(a: bigint, p: string): string {
+    this.numbers = [];
+    let program = p
+      .split(" ")[1]
+      .split(",")
+      .map((c) => parseInt(c));
+    // console.log(program);
+
+    let state = {
+      A: a,
+      B: 0n,
+      C: 0n,
+      ptr: 0,
+    };
+
+    while (state.ptr < program.length - 1) {
+      // console.log(state);
+      // console.log(
+      //   INSTRUCTION[program[state.ptr]] + " --- " + program[state.ptr + 1]
+      // );
+      let newState = this.performInstruction(
+        state,
+        program[state.ptr],
+        program[state.ptr + 1]
+      );
+      state = newState;
+      state.ptr += 2;
+    }
+    return this.numbers.join(",");
+  }
+
+  performInstruction(
+    state: State,
+    opcode: INSTRUCTION,
+    operand: number
+  ): State {
+    let advOp = (state: State, operand: number): bigint => {
+      let num: bigint = state.A;
+      let denom: bigint = 2n ** this.getOperand(state, operand);
+      let val: bigint = num / denom;
+      return val;
+    };
+    switch (opcode) {
+      case INSTRUCTION.ADV:
+        state.A = advOp(state, operand);
+        return state;
+      case INSTRUCTION.BXL:
+        state.B = state.B ^ BigInt(operand);
+        return state;
+      case INSTRUCTION.BST:
+        state.B = this.getOperand(state, operand) % 8n;
+        return state;
+      case INSTRUCTION.JNZ:
+        if (state.A === 0n) {
+          return state;
+        }
+        // console.log("JUMPING");
+        // Offset by 2 since we'll do += 2 after
+        state.ptr = operand - 2;
+        return state;
+      case INSTRUCTION.BXC:
+        state.B = state.B ^ state.C;
+        return state;
+
+      case INSTRUCTION.OUT:
+        // console.log(
+        //   `Adding ${this.getOperand(state, operand)} % 8 to the answer`
+        // );
+        let toOutput = this.getOperand(state, operand) % 8n;
+        this.numbers.push(toOutput);
+        return state;
+      case INSTRUCTION.BDV:
+        state.B = advOp(state, operand);
+        return state;
+
+      case INSTRUCTION.CDV:
+        state.C = advOp(state, operand);
+        return state;
+    }
+    return state;
+  }
+
+  getOperand(state: State, operand: number): bigint {
+    if (operand <= 3n) {
+      return BigInt(operand);
+    }
+    if (operand === 4) {
+      return state.A;
+    }
+    if (operand === 5) {
+      return state.B;
+    }
+    if (operand === 6) {
+      return state.C;
+    }
+    if (operand === 7) {
+      throw new Error("7 not to be used!");
+    }
+    throw new Error("Invalid operand: " + operand);
+  }
+
+  partTwo(input: string[]): any {
+    console.log("Trying to backtrack");
+    let potentialAns = this.partTwoNotADummy(input);
+
+    console.log("Backtrack done");
+    input[0] = `Register A: ${potentialAns}`;
+    let ans = this.partOne(input);
+    console.log(ans);
+  }
+
+  // Answer must be ~16 loops through this
+  // each time
+
+  // Operations [2, 4] + [1, 3] + [7, 5] + [4, 2] + [0, 3] + [1, 5] + [5, 5] + [3, 0]
+  //            bst      bxl      adc      bxc     adv     bxl       out       jnz
+  // B = A % 8
+  // B = B ^ 3
+  // C = A / 2^B
+  // B = B ^ C
+  // A = A / 2^3
+  // B = B ^ 5
+  // print last 3 bits of B
+  // jump back to start
+
+  // In summary:
+  // B takes last 3 bits of A.
+  // B then XORs with 011
+  // C is then
+  // Output = ((B^3)^C)^5
+
+  partTwoNotADummy(input: string[]): any {
+    let program = input[4]
+      .split(" ")[1]
+      .split(",")
+      .map((c) => parseInt(c));
+    console.log(program);
+
+    // Let's go through, 3 bits at a time and work on finding these numbers!
+    let ans = this.dumbBacktrack(program, 0, 0n);
+    console.log("Hypothetical ans");
+    console.log(ans);
+    return ans;
+  }
+
+  // B = A % 8
+  // B = B ^ 3
+  // C = A / 2^B
+  // B = B ^ C
+  // A = A / 2^3
+  // B = B ^ 5
+  dumbBacktrack(targetNums: number[], index: number, previous: bigint): bigint {
+    console.log(`${index} - ${previous}`);
+    let spacing = ("" + index).repeat(index);
+    if (index === targetNums.length) {
+      console.log("We did it!");
+      return previous;
+    }
+
+    for (let guess = 0n; guess <= 7; guess++) {
+      let previousBits = (previous << 3n) + guess;
+
+      // This guess is wrong!
+      let needToMatch = targetNums[targetNums.length - 1 - index];
+      let B = this.lazyEvaluate(previousBits);
+      if (B !== BigInt(needToMatch)) {
+        // console.log(
+        //   `${spacing} ${B} is not equal to ${needToMatch} (${guess})`
+        // );
+        continue;
+      } else {
+        // console.log(`${spacing}${B} IS equal to ${needToMatch} (${guess})`);
+      }
+
+      let maybeAns = this.dumbBacktrack(targetNums, index + 1, previousBits);
+      if (maybeAns !== 0n) {
+        return maybeAns;
+      }
+      // console.log("COMING OUT");
+    }
+
+    return 0n;
+  }
+
+  lazyEvaluate(a: bigint): bigint {
+    let e = this.eval(a, "Program: 2,4,1,3,7,5,4,2,0,3,1,5,5,5");
+    console.log(`${a} evaluated to ${e}`);
+    return BigInt(e);
+  }
+}
+
+let sol = new Day17_2024();
+
+// utils.runTest(sol, utils.ProblemParts.One);
+// utils.run(sol, utils.ProblemParts.One);
+// utils.runTest(sol, utils.ProblemParts.Two);
+utils.run(sol, utils.ProblemParts.Two);
